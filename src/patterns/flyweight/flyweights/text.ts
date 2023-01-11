af60d0c9280b434a288014c52cfd3bdbbc4d27df
Sean Merritt <sean.merritt@extend.com>
2023-01-11 12:57:30 -0600

    Remove unnecessary declaration on ITextFlyweight for name

--- src/patterns/flyweight/flyweights/text.ts
+++ src/patterns/flyweight/flyweights/text.ts
@@ -3,7 +3,6 @@ import { IFlyweight } from './flyweight'
 
 // Common interface for any text-related flyweights.
 export interface ITextFlyweight extends IFlyweight {
-  name: string
   fontFamily: string
   fontColor: string
   print: (value: string) => void
@@ -12,7 +11,7 @@ export interface ITextFlyweight extends IFlyweight {
 // Concrete flyweight implementation example. Contains the intrinsic data of a context
 // and the value passed to the operation, in this case print, is the extrinsic data.
 export class TextFlyweight implements ITextFlyweight {
-  constructor(readonly name: string, readonly fontFamily: string, readonly fontColor: string) { }
+  constructor(readonly name: string, readonly fontFamily: string, readonly fontColor: string) {}
   print(value: string) {
     // Print the text to the screen. We'd use the extrinsic data (value)
     // passed into this flyweight in the operation along with
